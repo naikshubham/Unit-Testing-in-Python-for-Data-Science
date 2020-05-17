36f64845887e334e2817edb085007880d4f7b13e
naikshubham <naikshubham55@gmail.com>
2020-05-17 23:57:15 +0530

    test_modules

--- test_modules/preprocessing_helpers.py
+++ test_modules/preprocessing_helpers.py
@@ -53,3 +53,18 @@ def split_into_training_and_testing_sets(array):
 #        [  1697., 277794.]])
 # )
 
+def convert_to_int_new(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with the correct exception for float valued argument strings
+    except ValueError:
+        return None
---
+++ test_modules/test_tdd_convert_to_init.py
@@ -0,0 +1,43 @@
+import pytest
+
+from preprocessing_helpers import convert_to_int_new as convert_to_int
+# TDD: Tests for normal arguments
+
+def test_with_no_comma():
+    actual = convert_to_int("756")
+    # Complete the assert statement
+    assert actual == 756, "Expected: 756, Actual: {0}".format(actual)
+    
+def test_with_one_comma():
+    actual = convert_to_int("2,081")
+    # Complete the assert statement
+    assert actual == 2081, "Expected: 2081, Actual: {0}".format(actual)
+    
+def test_with_two_commas():
+    actual = convert_to_int("1,034,891")
+    # Complete the assert statement
+    assert actual == 1034891, "Expected: 1034891, Actual: {0}".format(actual)
+
+# TDD: Requirement collection
+'''
+What should convert_to_int() do if the arguments are not normal? In particular, there are three special argument types:
+
+    Arguments that are missing a comma e.g. "178100,301".
+    Arguments that have the comma in the wrong place e.g. "12,72,891".
+    Float valued strings e.g. "23,816.92".
+'''
+
+# Give a name to the test for an argument with missing comma
+def test_on_string_with_missing_comma():
+    actual = convert_to_int("178100,301")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_string_with_incorrectly_placed_comma():
+    # Assign to the actual return value for the argument "12,72,891"
+    actual = convert_to_int("12,72,891")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+    
+def test_on_float_valued_string():
+    actual = convert_to_int("23,816.92")
+    # Complete the assert statement
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
